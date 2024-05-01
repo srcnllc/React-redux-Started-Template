20be155ac308d279e0703c460f8979b4035a1554
Sercan LÜLLECİ <srcn_llc_@hotmail.com>
2024-05-01 12:17:02 +0300

    routercomponent

--- src/App.js
+++ src/App.js
@@ -1,30 +1,10 @@
-import About from "./pages/About/About";
-import Contact from "./pages/Contact/Contact";
-import Home from "./pages/Home/Home";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-const router=createBrowserRouter([
-  {
-      path:"/",
-      element:<Home/>
-  },
-  {
-      path:"/about",
-      element:<About/>
-  },
-  {
-    path:"/contact",
-    element:<Contact/>
-}
-
-]);
+import { RouterProvider } from "react-router-dom";
+import {router} from "./components/RouterPage"
 
 function App() {
   return (
-    <div className="App">
       <RouterProvider router={router}/>
-    </div>
   );
 }
-
 export default App;
---
+++ src/components/RouterPage/index.js
@@ -0,0 +1,20 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../../pages/Home/Home";
+import About from "../../pages/About/About";
+import Contact from "../../pages/Contact/Contact";
+
+export const router=createBrowserRouter([
+    {
+        path:"/",
+        element:<Home/>
+    },
+    {
+        path:"/about",
+        element:<About/>
+    },
+    {
+      path:"/contact",
+      element:<Contact/>
+  }
+  
+  ]);
